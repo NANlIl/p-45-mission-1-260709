7c6587bb975f4362dd42cfd3a0296b14b329faac
Deokwoo Kim <ejrdn7298@gmail.com>
2026-07-09 14:23:25 +0900

    [BE14]김덕우

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        int age = 25;
+        String name = "김덕우";
+        System.out.println("이름:" + name);
+        System.out.println("나이:" + age);
+    }
+}
